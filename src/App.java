9ca082a67afb794be50a63c87c033a987ee1fc3a
Ibrahim Duman <dumanibrahm@gmail.com>
2023-02-01 19:20:04 +0300

    HackerRank Solution End of File

--- src/App.java
+++ src/App.java
@@ -106,5 +106,12 @@ public class App {
 
         }
     }
+    public static void endOfFile(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s ="";
+        for (int i = 1; sc.hasNext(); i++) {
+            System.out.println(i+" "+sc.nextLine());
+        }
+    }
 
 }
