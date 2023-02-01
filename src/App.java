ad2b0a784f9783e9d2abe6bf166c3ee0a364a520
Ibrahim Duman <dumanibrahm@gmail.com>
2023-02-01 19:13:15 +0300

    HackerRank Solution Java StdIn and StdOut

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        
+    }
+    public static void WelcomeToJava(){
         System.out.println("Hello, World!");
     }
+
+    public static void JavaStdinaAndOut(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println(scan.nextInt());
+    }
+        
 }
