45efb3b18281207eec4bb0a764d8657b18714e64
Ibrahim Duman <dumanibrahm@gmail.com>
2023-02-01 19:23:38 +0300

    HackerRank Solution Currency Formatter

--- src/App.java
+++ src/App.java
@@ -1,4 +1,7 @@
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.NumberFormat;
 import java.util.*;
 
 public class App {
@@ -121,5 +124,37 @@ public class App {
             System.out.println(s*s2);
         else
             System.out.println("java.lang.Exception: Breadth and height must be positive");
+    }
+    /*Java Int to String
+     * String s = ""+n;
+     */
+    public static String findDay(int month, int day, int year) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.MONTH,month-1);
+        cal.set(Calendar.DAY_OF_MONTH,day);
+        cal.set(Calendar.YEAR,year);
+        String dayOfWeek = cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.US).toUpperCase();
+        return dayOfWeek;
+    }
+    public static void CurrencyFormatter(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+       /* Create custom Locale for India. 
+          I used the "IANA Language Subtag Registry" to find India's country code */
+        Locale indiaLocale = new Locale("en", "IN");
+
+        /* Create NumberFormats using Locales */
+        NumberFormat us     = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat india  = NumberFormat.getCurrencyInstance(indiaLocale);
+        NumberFormat china  = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+
+        /* Print output */        
+        System.out.println("US: "     + us.format(payment));
+        System.out.println("India: "  + india.format(payment));
+        System.out.println("China: "  + china.format(payment));
+        System.out.println("France: " + france.format(payment));
     }
 }
