22a4923df27ed9ff181b8898b7ec76ae82c4d940
Ibrahim Duman <dumanibrahm@gmail.com>
2023-02-01 19:18:08 +0300

    HackerRank Solution Java Loops 2

--- src/App.java
+++ src/App.java
@@ -67,6 +67,25 @@ public class App {
         }
         bufferedReader.close();
     }
+    public static void Loops2(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int sum = a;
+            for (int j = 0; j<n; j++) {                
+                int nextEle = (int)Math.pow(2, j)*b;
+                sum = sum + nextEle;
+                System.out.printf("%s ",sum);                                
+            }
+            if (i < t-1) {
+                System.out.print("\n");
+            }
+        }
+        in.close();
+    }
 
         
 }
